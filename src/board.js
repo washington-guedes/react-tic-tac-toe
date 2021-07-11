12e0a77d9cb3abc0e5463f77bd3abbe00e0dce30
Washington Guedes <avwl017@gmail.com>
2021-07-11 17:02:29 -0300

    build board with loops intead of hardcoded squares

--- src/board.js
+++ src/board.js
@@ -7,29 +7,16 @@ export class Board extends React.Component {
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        key={i}
       />
     );
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+    return [...Array(3)].map((_, i) => (
+      <div className="board-row" key={i}>
+        {[...Array(3)].map((_, j) => this.renderSquare(i*3 + j))}
       </div>
-    );
+    ));
   }
 }
